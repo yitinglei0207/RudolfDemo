bb08c2f519407c8871b98cce72e264d120c0aa1d
yitinglei <yitinglei0207@gmail.com>
2015-04-28 10:51:48 +0800

    Rudolf first commit

---
+++ RudolfDemo/MapViewController.h
@@ -0,0 +1,14 @@
+//
+//  MapViewController.h
+//  RudolfDemo
+//
+//  Created by Jason Lei on 2015/4/24.
+//  Copyright (c) 2015年 AlphaCamp. All rights reserved.
+//
+#import <MapKit/MapKit.h>
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+@property (weak, nonatomic) IBOutlet MKMapView *map;
+
+@end
---
+++ RudolfDemo/MapViewController.m
@@ -0,0 +1,37 @@
+//
+//  MapViewController.m
+//  RudolfDemo
+//
+//  Created by Jason Lei on 2015/4/24.
+//  Copyright (c) 2015年 AlphaCamp. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
